5db77d56aed00c068712b2199f626b3ddd015928
aman-codde <amandeepk221@gmail.com>
2022-11-15 09:37:23 -0800

    install(): install swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder();
+  await app.listen(3001);
 }
 bootstrap();
