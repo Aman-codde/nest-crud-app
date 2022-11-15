61d2ec39550c7698c5545153d65f855fe354b366
aman-codde <amandeepk221@gmail.com>
2022-11-15 09:45:24 -0800

    feat(main.ts): add configuration to enable swagger

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,19 @@
 import { NestFactory } from '@nestjs/core';
-import { DocumentBuilder } from '@nestjs/swagger';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const config = new DocumentBuilder();
+  const config = new DocumentBuilder()
+    .setTitle('Nest Api')
+    .setDescription('the description of API')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('swagger', app, document);
   await app.listen(3001);
 }
 bootstrap();
